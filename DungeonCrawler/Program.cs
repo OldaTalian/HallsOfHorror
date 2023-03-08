3d8738c6e9d625db13fca445832ce4e06d11b734
OldaTalian <114180423+OldaTalian@users.noreply.github.com>
2023-03-08 16:52:19 +0100

    move

--- DungeonCrawler/Program.cs
+++ DungeonCrawler/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace DungeonCrawler
+namespace DungeonCrawler
 {
     internal class Program
     {
+        public static void Move()
+        {
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.UpArrow: // NAHORU
+                case ConsoleKey.W:
+                    Console.WriteLine("up");
+                    break;
+                case ConsoleKey.DownArrow: // DOLŮ
+                case ConsoleKey.S:
+                    Console.WriteLine("down");
+                    break;
+                case ConsoleKey.LeftArrow: // DOLEVA
+                case ConsoleKey.A:
+                    Console.WriteLine("left");
+                    break;
+                case ConsoleKey.RightArrow: // DOPRAVA
+                case ConsoleKey.D:
+                    Console.WriteLine("right");
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+                
+            while(true)
+            {
+                Move();
+            }
+            
         }
+    
     }
 }
