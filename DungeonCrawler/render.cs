38d4c3ef025a28b27c929fdacacae00a19ea62e9
Oldřich Talián <o.talian@student.nosch.cz>
2023-03-24 10:03:02 +0100

    Circle vision

--- DungeonCrawler/render.cs
+++ DungeonCrawler/render.cs
@@ -6,42 +6,130 @@ namespace DungeonCrawler
 {
     internal class render
     {
-        public static void Render() // This is a function that renders the GAME
+        public static void Render()
         {
             Console.Clear();
             if (Map == null) return;
 
-            Console.SetWindowSize(Map[0].Length + 3, Map.Length + 1);
-
             Console.WriteLine("Rooms: " + ThisRoom + 1 + "/" + AllRooms().Length + " |  x:" + FindPlayer()[0] + " y:" + FindPlayer()[1] +
-               " Health: " + ((playerHealth > 0) ? playerHealth : 0)); // Vypisuje v jaké místnosti z kolika hráč je (pro debug)
+                " Health: " + ((playerHealth > 0) ? playerHealth : 0));
 
-            // Renders map to console
-            for (int i = 0; i < Map.Length; i++)
+            // Check if there is a circle to be drawn
+            int maxDistance;
+            if (Map[Map.Length - 1][Map[Map.Length - 1].Length - 2] == 'Đ' &&
+                int.TryParse(Map[Map.Length - 1][Map[Map.Length - 1].Length - 1].ToString(), out maxDistance))
             {
-                for (int j = 0; j < Map[i].Length; j++)
+                // Renders map with circle
+                int[] playerPos = FindPlayer();
+
+                for (int i = 0; i < Map.Length; i++)
                 {
-                    if (Map[Map.Length - 1][Map[0].Length - 2] == 'Đ') // special check for "Đ" tile
+                    for (int j = 0; j < Map[i].Length; j++)
                     {
-                        int maxDistance = Convert.ToInt32(new string(Map[Map.Length - 1][Map[Map.Length - 1].Length - 1], 1));
-                        int[] pos = { j, i };
-                        if (MeasureDistance(FindPlayer(), pos) > maxDistance) // if distance to player > maxDistance, render space instead
+                        int[] tilePos = { j, i };
+                        int distance = MeasureDistance(playerPos, tilePos);
+
+                        if (distance <= maxDistance)
                         {
-                            Console.Write(' ');
+                            // Calculate the radius of the circle that should be drawn around the player position
+                            int radius = maxDistance - distance + 3;
+
+                            // Check if the tile is within the circle
+                            if (radius * radius >= Math.Pow(playerPos[0] - tilePos[0], 2) + Math.Pow(playerPos[1] - tilePos[1], 2))
+                            {
+                                // Check if there is a wall between the player and the tile
+                                bool isWallInBetween = false;
+                                foreach (var wallPos in GetTilesOnLine(playerPos, tilePos))
+                                {
+                                    if (Map[wallPos[1]][wallPos[0]] == '█')
+                                    {
+                                        isWallInBetween = true;
+                                        break;
+                                    }
+                                }
+
+                                if (isWallInBetween)
+                                {
+                                    if (Map[i][j] == '█')
+                                    {
+                                        Console.Write('█'); // its the wall
+                                    }
+                                    else
+                                    {
+                                        Console.Write(' '); // tile is behind a wall
+                                    }
+                                }
+                                else
+                                {
+                                    convertToMap(Map, j, i); // render the tile normally
+                                }
+                            }
+
+                            else
+                            {
+                                Console.Write(' '); // render space instead
+                            }
                         }
                         else
                         {
-                            convertToMap(Map, j, i); // otherwise, render the tile normally
+                            Console.Write(' '); // render space instead
                         }
                     }
-                    else
-                    {
-                        convertToMap(Map, j, i); // render tile normally
-                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
+            }
+            else
+            {
+                // Renders map without circle
+                for (int i = 0; i < Map.Length; i++)
+                {
+                    for (int j = 0; j < Map[i].Length; j++)
+                    {
+                        convertToMap(Map, j, i);
+                    }
+                    Console.WriteLine();
+                }
             }
         }
+
+        // Returns an array of tiles between the start and end points, inclusive, on a line defined by those points.
+        // using Bresenham's algorithm (https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm).
+        public static int[][] GetTilesOnLine(int[] startPos, int[] endPos)
+        {
+            int dx = Math.Abs(endPos[0] - startPos[0]);  // Calculate the absolute difference in x-coordinates.
+            int dy = Math.Abs(endPos[1] - startPos[1]);  // Calculate the absolute difference in y-coordinates.
+            int x = startPos[0];  // Initialize the current x-coordinate to the starting x-coordinate.
+            int y = startPos[1];  // Initialize the current y-coordinate to the starting y-coordinate.
+            int xStep = (endPos[0] > startPos[0]) ? 1 : -1;  // Calculate the step to be taken in the x-direction.
+            int yStep = (endPos[1] > startPos[1]) ? 1 : -1;  // Calculate the step to be taken in the y-direction.
+            int maxDistance = Math.Max(dx, dy) + 1;  // Calculate the maximum distance between the start and end points.
+            int[][] tiles = new int[maxDistance][];  // Create a new array to store the tiles.
+
+            int i = 0;  // Initialize the index variable.
+            while (i < maxDistance)
+            {
+                tiles[i] = new int[] { x, y };  // Store the current tile in the array.
+                i++;
+
+                int error = dx - dy;  // Calculate the error term.
+
+                // Take the appropriate step based on the error term.
+                if (error > 0 || (error == 0 && xStep > 0))
+                {
+                    x += xStep;
+                    error -= dy;
+                }
+                else
+                {
+                    y += yStep;
+                    error += dx;
+                }
+            }
+
+            return tiles;  // Return the array of tiles.
+        }
+
+
 
         public static bool skipNextOne = false;
 
